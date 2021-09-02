30053d77fe3d73d27eddcaaa1fe77ebe289ff381
Cole Fortner <colefortner@gmail.com>
2021-09-02 17:43:05 -0400

    Add validation for non-negative and non-numerical input

--- src/areaofacircle/Area.java
+++ src/areaofacircle/Area.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
 
+        String validation = "Please enter a non-negative number.";
+
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        Double radius = input.nextDouble();
-//        Double area = 3.14 * radius * radius;
-//        Double area = Circle.getArea(radius);
-        System.out.println("The area of a circe of radius " + radius + " is: " + Circle.getArea(radius));
+
+        if(!input.hasNextDouble()){
+            System.out.println(validation);
+        } else {
+            Double radius = input.nextDouble();
+            if(radius < 0){
+                System.out.println(validation);
+            } else {
+                System.out.println("The area of a circe of radius " + radius + " is: " + Circle.getArea(radius));
+            }
+        }
     }
 }
+
+//        Double area = 3.14 * radius * radius;
+//        Double area = Circle.getArea(radius);
